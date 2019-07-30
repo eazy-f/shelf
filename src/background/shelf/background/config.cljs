6850177b1f4f668cf445f7374b2a6e509ecafa27
Nikita Lebedev <eazy-f@yandex.ru>
2019-07-30 21:58:51 +0300

    Encrypt stored config with PIN

--- src/background/shelf/background/config.cljs
+++ src/background/shelf/background/config.cljs
@@ -1,6 +1,6 @@
 (ns shelf.background.config
   (:require-macros [cljs.core.async :refer [go-loop go]])
-  (:require [cljs.core.async :refer [chan >! <! mult]]
+  (:require [cljs.core.async :refer [chan >! <! mult promise-chan]]
             [clojure.string]
             [chromex.protocols.chrome-storage-area :as browser-storage]))
 
@@ -10,12 +10,76 @@
    :password "pass"})
 
 (defn- decrypt [stored]
+  (let [crypto js/window.crypto])
   (.parse js/JSON (clojure.string/reverse stored)))
+
+(defn- text-to-buffer [text]
+  (.encode (js/TextEncoder.) text))
+
+(defn- hex-to-buffer [string]
+  (let [buffer-length (quot (count string) 2)
+        pairs (partition 2 string)
+        array (js/Uint8Array. buffer-length)]
+    (dorun
+     (map-indexed
+      (fn [i [h l]]
+        (aset array i (js/parseInt (str h l) 16)))
+      pairs))
+    array))
+
+(defn- buffer-to-hex [buffer]
+  (apply
+   str
+   (for [oct (array-seq buffer)] (.padStart (.toString oct 16) 2 "0"))))
+
+(defn- encrypt [config pin salt]
+  (let [iv (.getRandomValues js/crypto (js/Uint8Array. 16))
+        plaintext (.stringify js/JSON (clj->js config))
+        cipher #js{:name "AES-CBC" :iv iv}
+        encoded-pin (text-to-buffer pin)
+        master-promise (promise-chan)]
+  (go
+    (.then
+     (.importKey
+      js/crypto.subtle
+      "raw"
+      encoded-pin
+      "PBKDF2"
+      false
+      #js["deriveKey"])
+     #(go (>! master-promise %1)))
+    (let [master-key (<! master-promise)
+          key-promise (promise-chan)]
+      (.then
+       (.deriveKey
+        js/crypto.subtle
+        #js{:name "PBKDF2"
+            :salt salt
+            :iterations 100000
+            :hash "SHA-512"}
+        master-key
+        #js{:name "AES-CBC" :length 256}
+        true
+        #js["encrypt" "decrypt"])
+       #(go (>! key-promise %1)))
+      (let [encryption-key (<! key-promise)
+            ciphertext-promise (promise-chan)]
+        (.then
+         (.encrypt
+          js/crypto.subtle
+          cipher
+          encryption-key
+          (text-to-buffer plaintext))
+         #(go (>! ciphertext-promise %1)))
+        (let [ciphertext (<! ciphertext-promise)]
+          (buffer-to-hex (js/Uint8Array. ciphertext))))))))
 
 (defn- load-stored-state [storage]
   (go
     (if-some [stored (.-config (first (first (<! (browser-storage/get storage)))))]
-      (into (js->clj (decrypt stored)) {:type "configured"})
+      (do
+        (println "read:" stored)
+        {:type "configured"})
       EMPTYSTATE)))
 
 (defn- clear-storage [storage]
@@ -27,8 +91,8 @@
 (defn- generate-encryption-key []
   "Donhirch0Ow2")
 
-(defn- encrypt [config pin]
-  (clojure.string/reverse (.stringify js/JSON (clj->js config))))
+(defn- generate-salt []
+  (.getRandomValues js/crypto (js/Uint8Array. 16)))
 
 (defn- configure-storage [storage]
   (go
@@ -38,13 +102,15 @@
                               {:type "active"
                                :stg-key stg-encryption-key
                                :pin pin})
-          encrypted-config (encrypt stored-config pin)]
-      ;FIXME: double check this read
-      (when (<! (browser-storage/set storage (clj->js {:config encrypted-config})))
+          salt (generate-salt)
+          encrypted-config (<! (encrypt stored-config pin salt))
+          stored-objects {:config encrypted-config :config-salt (buffer-to-hex salt)}]
+      ;;FIXME: double check this read
+      (when (<! (browser-storage/set storage (clj->js stored-objects)))
         (into stored-config {:pin pin})))))
 
 (defn- try-activate [state pin]
-  (if (= pin (state "pin"))
+  (if (= pin (:pin state))
     {:type "active" :pin pin}
     state))
 
@@ -53,12 +119,13 @@
 
 (defn- handle-command [cmd state storage]
   (go
-    (let [[cmd-name & args] cmd]
+    (let [[cmd-name & args] cmd
+          fresh-start (fn [] (go (<! (clear-storage storage)) (<! (load-stored-state storage))))]
       (case [(:type state) cmd-name]
         ["empty" "configure"] (<! (configure-storage storage))
         ["configured" "activate"] (apply try-activate state args)
-        ["configured" "clear"] (do (<! (clear-storage storage)) (<! (load-stored-state storage)))
-        ["active" "clear"] (do (<! (clear-storage storage)) (<! (load-stored-state storage)))
+        ["configured" "clear"] (<! (fresh-start))
+        ["active" "clear"] (<! (fresh-start))
         ["active" "logout"] (logout state)
         state))))
 
