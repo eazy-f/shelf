6576dcc8e9968968a49d7bb28e1f4da4dafbb90a
Nikita Lebedev <eazy-f@yandex.ru>
2019-07-30 13:15:46 +0300

    Add popup state machine

--- src/background/shelf/background/status.cljs
+++ src/background/shelf/background/status.cljs
@@ -3,12 +3,39 @@
             [chromex.ext.runtime :as runtime]
             [chromex.protocols.chrome-port :refer [get-name post-message!]]))
 
+(def EMPTYSTATE {:type "empty"})
+
+(defn- get-current-state []
+  {:type "empty"})
+
+(defn- mockup-active []
+  {:type "active" :pin "1111"})
+
+(defn- try-activate [state pin]
+  (if (= pin (:pin state))
+    {:type "active" :pin pin}
+    state))
+
+(defn- logout [state]
+  {:type "configured" :pin (:pin state)})
+
+(defn- handle-command [cmd state]
+  (let [[cmd-name & args] cmd]
+    (case [(:type state) cmd-name]
+      ["empty" "configure"] (mockup-active)
+      ["configured" "activate"] (apply try-activate state args)
+      ["configured" "clear"] EMPTYSTATE
+      ["active" "clear"] EMPTYSTATE
+      ["active" "logout"] (logout state)
+      state)))
+
 (defn- serve-client [port]
-  (go-loop []
-    (when-some [msg (<! port)]
-      (println msg)
-      (post-message! port (clj->js {:type :empty}))
-      (recur))))
+  (post-message! port (clj->js (get-current-state)))
+  (go-loop [state EMPTYSTATE]
+    (when-some [cmd (<! port)]
+      (let [new-state (handle-command cmd state)]
+        (post-message! port (clj->js new-state))
+        (recur new-state)))))
 
 (defn listen []
   (let [connections (chan)]
--- src/popup/shelf/popup.cljs
+++ src/popup/shelf/popup.cljs
@@ -1,6 +1,7 @@
 (ns shelf.popup
   (:require-macros [chromex.support :refer [runonce]])
   (:require [cljs.core.async :refer [<! >! go-loop]]
+            [goog.dom :as gdom]
             [chromex.ext.runtime :as runtime]
             [chromex.protocols.chrome-port :refer [post-message!]]))
 
@@ -10,42 +11,93 @@
 (defn- get-popup-area []
   (js/document.getElementById "popup-area"))
 
-(defn- set-content [element]
-  (let [parent (get-popup-area)]
-    (.replaceChild parent element (get-doc-element "settings-buttons-set"))))
+(defn- get-doc-element [id]
+  (js/document.getElementById id))
 
 (defn- get-first-by-tag [parent tag]
   (.item (.getElementsByTagName parent tag) 0))
 
-(defn- activate-buttons [buttons-set]
-  (let [fieldset (get-first-by-tag buttons-set "fieldset")]
-    (.removeAttribute fieldset "disabled")
-    buttons-set))
+(defn- set-content [element]
+  (let [parent (get-popup-area)]
+    (loop []
+      (when-some [child (.-firstChild parent)]
+        (.removeChild parent child)
+        (recur)))
+    (.appendChild parent element)))
 
-(defn- get-doc-element [id]
-  (js/document.getElementById id))
+(defn- activate-buttons [port]
+  (let [make-button #(gdom/createDom "button" #js{:type "button"} %1)
+        configure (make-button "Configure")
+        clear (make-button "Clear")
+        form (gdom/createDom "form" nil configure)]
+    (.addEventListener
+     configure
+     "click"
+     (fn [event] (port-send port ["configure"])))
+    (.addEventListener
+     clear
+     "click"
+     (fn [event] (port-send port ["clear"])))
+    form))
+
+(defn- get-form-values [event]
+  (into
+   {}
+   (for
+       [e (array-seq (.-elements (.-target event)))
+        :while (= "text" (.-type e))]
+     [(keyword (.-name e)) (.-value e)])))
+
+(defn- build-pin-code-form [port]
+  (let [button (gdom/createDom "button" #js{:type "submit"} "Enter")
+        input (gdom/createDom "input" #js{:name "pin" :type "text" :placeholder "PIN"})
+        form (gdom/createDom "form" nil input button)]
+    (.addEventListener form
+                       "submit"
+                       (fn [event]
+                         (let [{:keys [pin]} (get-form-values event)]
+                           (port-send port ["activate" pin])
+                           (.preventDefault event))))
+    form))
+
+(defn- show-active [state port]
+  (let [logout (gdom/createDom "button" #js{:type "button"} "Logout")
+        text (gdom/createDom "h6" nil (.-pin state))]
+    (.addEventListener
+     logout
+     "click"
+     (fn [event] (port-send port ["logout"])))
+    (gdom/createDom "form" nil text logout)))
+
+;; < empty
+;; > configure
+;; < active+auto-pin
+;; > show-pin+logout
+;; > logout
+;; < configured
+;; > pin
+;; < active+pin
 
 (defn- update-buttons [port buttons]
   (let [root-area (get-popup-area)
-        starting-elements (get-doc-element "settings-buttons-set")
-        active-buttons (activate-buttons (.cloneNode starting-elements true))]
+        starting-elements (get-doc-element "startup-placeholder")
+        active-buttons (activate-buttons port)
+        pin-code-form (build-pin-code-form port)]
     (go-loop [old-state-type nil]
       (when-some [state (<! port)]
         (let [state-type (.-type state)]
           (when (not= old-state-type state-type)
-            (when-let [popup-elements
-                       (case state-type
-                         "empty" active-buttons
-                         "configured" starting-elements
-                         starting-elements)]
-              (set-content popup-elements))
-        (recur state-type)))))))
+            (set-content (case state-type
+                           "empty" active-buttons
+                           "configured" pin-code-form
+                           "active" (show-active state port)
+                           starting-elements)))
+        (recur state-type))))))
 
 (defn update-status []
   (let [cinfo (clj->js {:name "status"})
         port (runtime/connect :omit cinfo)
         buttons (get-doc-element "buttons")]
-    (port-send port {:type :test})
     (update-buttons port buttons)))
 
 (runonce (update-status))
